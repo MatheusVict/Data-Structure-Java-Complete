c78fe7ba8b9efa9feaf9b4cd849acf1919a723bc
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-08 20:40:12 -0300

    feat: busca linear
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/io/matheusvictor/dataStructure/vetor/LinearSearch.java
@@ -0,0 +1,29 @@
+package io.matheusvictor.dataStructure.vetor;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] vector = new int[8];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 10);
+            System.out.println(vector[i]);
+        }
+
+        System.out.println("Choose a number");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int terminal = scanner.nextInt();
+        boolean found = false;
+
+        for (int j : vector) {
+            if (j == terminal) {
+                found = true;
+                break;
+            }
+        }
+        if (found) System.out.println("i found it");
+        else System.out.println("nope");
+    }
+}
