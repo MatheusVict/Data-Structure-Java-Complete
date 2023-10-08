03ece33db39297ef89d56269909d3f6bc1f73f51
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-08 17:59:22 -0300

    feat: basic comparation
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/io/matheusvictor/dataStructure/listaligada/Comparation.java
@@ -0,0 +1,57 @@
+package io.matheusvictor.dataStructure.listaligada;
+
+import java.util.ArrayList;
+
+public class Comparation {
+    public static void main(String[] args) {
+        ListaLigada<Integer> listaLigada = new ListaLigada<>();
+
+        ArrayList<Integer> vector = new ArrayList<>();
+
+        long limit = 1000;
+        long initialTime = System.currentTimeMillis();
+        long finalTime;
+
+        for (int i = 0; i < limit; i++) {
+            vector.add(i);
+        }
+
+        finalTime = System.currentTimeMillis();
+        System.out.println("JDK add: \n");
+        System.out.println(finalTime - initialTime);
+
+         initialTime = System.currentTimeMillis();
+
+        for (int i = 0; i < limit; i++) {
+            listaLigada.addNewElement(i);
+        }
+
+        finalTime = System.currentTimeMillis();
+        System.out.println("my implementation add: \n");
+        System.out.println(finalTime - initialTime);
+
+        // ready from linkedList and read from vector
+
+        initialTime = System.currentTimeMillis();
+
+        for (int i = 0; i < vector.size(); i++) {
+            vector.get(i);
+        }
+
+        finalTime = System.currentTimeMillis();
+        System.out.println("JDK get:\n");
+        System.out.println(finalTime - initialTime);
+
+        initialTime = System.currentTimeMillis();
+        System.out.println("before running");
+
+        for (int i = 0; i < listaLigada.getSize(); i++) {
+            listaLigada.getElement(i);
+        }
+        System.out.println("after");
+
+        finalTime = System.currentTimeMillis();
+        System.out.println("My implementation get:\n");
+        System.out.println(finalTime - initialTime);
+    }
+}
